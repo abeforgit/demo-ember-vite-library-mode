b54861c9ea5bded84f493d81f4399797003c30b9
abeforgit <arnebertrand@gmail.com>
2025-03-27 21:10:09 +0100

    initial commit

---
+++ tests/test-helper.ts
@@ -0,0 +1,15 @@
+import Application from 'embeddable-test-vite/app';
+import config from 'embeddable-test-vite/config/environment';
+import * as QUnit from 'qunit';
+import { setApplication } from '@ember/test-helpers';
+import { setup } from 'qunit-dom';
+import { start as qunitStart, setupEmberOnerrorValidation } from 'ember-qunit';
+
+export function start() {
+  setApplication(Application.create(config.APP));
+
+  setup(QUnit.assert);
+  setupEmberOnerrorValidation();
+
+  qunitStart();
+}
